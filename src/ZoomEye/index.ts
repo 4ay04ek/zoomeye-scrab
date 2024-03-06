c9a5721216f5100afe56f41d43466bca3a9dc41d
4ay04ek <48386123+4ay04ek@users.noreply.github.com>
2024-03-06 12:37:29 +0300

    Update index.ts

--- src/ZoomEye/index.ts
+++ src/ZoomEye/index.ts
@@ -2,70 +2,70 @@ import axios from "axios";
 import { sleep } from "../helpers";
 
 const REGION = "RU";
-const PROTOCOLS_TO_CHECK = ["postgresql", "elasticsearch", "mongodb"];
+const PROTOCOLS_TO_CHECK = ["elasticsearch", "mongodb", "postgresql"];
 const BASIC_SEARCH_URL = "https://www.zoomeye.org/api/search";
 const BASIC_SEARCH_TOTAL_URL = "https://www.zoomeye.org/api/search_total";
 const PAGE_SIZE = 50;
 
-const formQuery = (from_days, to_days) => {
-  let to = new Date();
-  to.setDate(to.getDate() - to_days);
-  let to_date = to.toISOString().substring(0, 10);
-
-  let from = new Date();
-  from.setDate(from.getDate() - from_days);
-  let from_date = from.toISOString().substring(0, 10);
-
-  console.log("ZoomEye scanning from " + from_date + " to " + to_date);
+const formQuery = (time_start, time_end) => {
+  console.log("ZoomEye scanning from " + time_start + " to " + time_end);
 
   return `(${PROTOCOLS_TO_CHECK.map((p) => `service:${p}`).join(
     " "
-  )}) +country:${REGION} +after:"${from_date}" +before:"${to_date}"`;
+  )}) +country:${REGION} +after:"${time_start}" +before:"${time_end}"`;
 };
 
-type HostWithoutId = {
-  organization: string;
-  service: string;
-  ip: string;
-  date_added: Date;
-};
+const formDate = (hours) => {
+    let date = new Date(new Date(new Date().toISOString().substring(0, 10)));
+    date.setTime(date.getTime() - hours * 60 * 60 * 1000);
+    return `${date.toISOString().substring(0, 10)} ${date.getUTCHours()}:00`
+}
 
-export default async (from_days, to_days): Promise<HostWithoutId[]> => {
-  let q = formQuery(from_days, to_days);
-  let total = (
-    await axios.get(BASIC_SEARCH_TOTAL_URL, {
-      params: {
-        q,
-        page: 1,
-      },
-      headers: {
-        "Cube-Authorization": process.env.ZOOMEYE_KEY,
-      },
-    })
-  ).data.total;
-  console.log("ZoomEye scanned " + total);
-  let raw = [];
-  for (let i in Array.from(Array(Math.min(Math.ceil(total / PAGE_SIZE), 8)))) {
-    let response = await axios
-      .get(BASIC_SEARCH_URL, {
+const sleep = ms => new Promise(r => setTimeout(r, ms));
+
+const func = async () => {
+    let result = [];
+  for (let hours_ago = 24; hours_ago > 0; hours_ago -= 1) {
+    const start = formDate(hours_ago), end = formDate(hours_ago - 1);
+    let q = formQuery(start, end);
+    console.log("Used query " + q)
+    let total = (
+        await axios.get(BASIC_SEARCH_TOTAL_URL, {
         params: {
-          q,
-          page: parseInt(i) + 1,
-          pageSize: PAGE_SIZE,
+            q,
+            page: 1,
         },
         headers: {
-          "Cube-Authorization": process.env.ZOOMEYE_KEY,
+            "Cube-Authorization": process.env.ZOOMEYE_KEY,
         },
-      })
-      .then((r) => ([403, 429].includes(r.data.status) ? console.log("ZoomEye Error") : r.data.matches || []));
-    if (response) raw = [...raw, ...response];
-    await sleep(1000);
-  }
-  console.log("Total ZoomEye received " + raw.length);
-  return raw.map((v) => ({
-    ip: Array.isArray(v.ip) ? v.ip[0] : v.ip,
-    organization: v.geoinfo.organization,
-    service: v.portinfo.service,
-    date_added: new Date(v.timestamp),
-  }));
+        }).then((r) => ([403, 429].includes(r.data.status) ? console.log("ZoomEye Error") : r || null))
+    ).data.total;
+    if (!total) continue;
+    console.log("ZoomEye scanned " + total);
+    let raw = [];
+    for (let i in Array.from(Array(Math.min(Math.ceil(total / PAGE_SIZE), 8)))) {
+        let response = await axios
+        .get(BASIC_SEARCH_URL, {
+            params: {
+            q,
+            page: parseInt(i) + 1,
+            pageSize: PAGE_SIZE,
+            },
+            headers: {
+            "Cube-Authorization": process.env.ZOOMEYE_KEY,
+            },
+        })
+        .then((r) => ([403, 429].includes(r.data.status) ? console.log("ZoomEye Error") : r.data.matches || []));
+        if (response) raw = [...raw, ...response];
+        await sleep(1000);
+    }
+        result = [...result, ...raw.map((v) => ({
+            ip: Array.isArray(v.ip) ? v.ip[0] : v.ip,
+            organization: v.geoinfo.organization,
+            service: v.portinfo.service,
+            date_added: new Date(v.timestamp),
+        }))];
+    }
+    console.log(result);
+    console.log("Total get " + result.length);
 };
